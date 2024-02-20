18eb0b8d899c37be8df59a6be0c45eefb01e77ad
sallyMohamed <engsally@gmail.com>
2024-02-20 07:19:52 -0800

    done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 def display_hello():
     return "Hello HBNB!"
 
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def display_hello():
+    """Prints hello hbnb"""
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
